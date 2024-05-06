e1caa4be18c92fc4493ca7d2581c41f18b9048ea
Utku ULU <utku.ulu@bosphorusiss.com>
2024-05-06 15:38:47 +0300

    refactor: Update landing page styles and button link

--- src/Hero/LandingHero.jsx
+++ src/Hero/LandingHero.jsx
@@ -8,10 +8,13 @@ function LandingHero() {
         <div className="hero-cta-card-title-container">
           <h1 className="hero-cta-card-title">PawSelect</h1>
           <h2 className="hero-cta-card-subtitle">
-            Select Your Image with Magic
+            Choose Your Images with Magic
           </h2>
           <p className="hero-cta-card-description">
-            PawPicker simplifies your quest for the perfect image. Instead of overwhelming choices, our enchanting wizard rabbit guides you through a magical A/B testing process. Just choose the better of two images, and continue selecting until you uncover the best one. It’s an effortless way to make decisions and ensure your final choice is the crowd favorite.
+            PawPicker simplifies your quest for the perfect image. Our
+            enchanting wizard rabbit guides you through a magical A/B testing
+            process. It’s an effortless way to make decisions and ensure your
+            final choice is the crowd favorite.
           </p>
           <Link to="/tester">
             <button className="hero-cta-card-button-movie">
