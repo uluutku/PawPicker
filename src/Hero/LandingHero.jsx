2b4c423f998a0a276fa99ce7f123a9dad6aa18dd
Utku ULU <utku.ulu@bosphorusiss.com>
2024-05-06 11:46:49 +0300

    refactor: Update landing page subtitle and button text

--- src/Hero/LandingHero.jsx
+++ src/Hero/LandingHero.jsx
@@ -9,17 +9,14 @@ function LandingHero() {
         <div className="hero-cta-card-title-container">
           <h1 className="hero-cta-card-title">PawSelect</h1>
           <h2 className="hero-cta-card-subtitle">
-            En Popüler Filmler ve Diziler
+            Select Your Image with Magic
           </h2>
           <p className="hero-cta-card-description">
-            RabbitFlix, en popüler filmleri ve dizileri sizin için bir araya
-            getiren bir NetFlix klonu projesidir. Frontend becerilerini
-            geliştirmek amacı ile yapılmış bu proje, The Movie Database API'ını
-            kullanarak en popüler filmleri ve dizileri listeler.
+            PawPicker simplifies your quest for the perfect image. Instead of overwhelming choices, our enchanting wizard rabbit guides you through a magical A/B testing process. Just choose the better of two images, and continue selecting until you uncover the best one. It’s an effortless way to make decisions and ensure your final choice is the crowd favorite.
           </p>
           <Link to="/movies">
             <button className="hero-cta-card-button-movie">
-              Filmleri İncele
+              Cast a Selection Spell
             </button>
           </Link>
         </div>
